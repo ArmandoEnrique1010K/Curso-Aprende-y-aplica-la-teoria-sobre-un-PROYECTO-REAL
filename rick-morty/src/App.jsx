17962bb726fcad39a91fd3628e982c513d6cdb6a
ArmandoEnrique1010K <enrique1010k@gmail.com>
2024-10-22 21:59:27 -0500

    proyecto original del curso

--- rick-morty/src/App.jsx
+++ rick-morty/src/App.jsx
@@ -1,54 +1,35 @@
-// Solamente importa la imagen si se encuentra dentro de la carpeta src
-// import imageRickMorty from './img/rick-morty.png'
-import './App.css'
-import { useEffect, useState } from 'react';
-import Characters from './components/Characters';
+import imageRickMorty from "./img/rick-morty.png";
+import "./App.css";
+import { useState } from "react";
+import Characters from "./Components/Characters";
 
 function App() {
   const [characters, setCharacters] = useState(null);
 
-  const [infoApi, setInfoApi] = useState(null)
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
 
-  const [url, setUrl] = useState('https://rickandmortyapi.com/api/character')
-
-  // Funcion asincrona
-  const requestApi = async () => {
-    const api = await fetch(url);
-    const characterApi = await api.json();
-    console.log(characterApi);
-    setInfoApi(characterApi.info);
-    setCharacters(characterApi.results);
-
-    // https://rickandmortyapi.com/
-    // web oficial de la API Rick and Morty
-  }
-
-  useEffect(() => {
-    requestApi()
-    console.log("url cambio a " + url)
-  }, [url])
-
+    const charactersApi = await api.json();
+    setCharacters(charactersApi.results);
+  };
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='title'>Rick & Morty</h1>
-        {
-          characters ? (
-            <Characters characters={characters} setCharacters={setCharacters} setUrl={setUrl} infoApi={infoApi} setInfoApi={setInfoApi} />
-          ) : (
-            <>
-              {/* <img src={imageRickMorty} alt="Rick & Morty" className='img-home' /> */}
-              <img src={"./img/rick-morty2.png"} alt="Rick & Morty" className='img-home' />
-
-            </>
-          )
-        }
-        {/* Boton para buscar */}
-        <button onClick={requestApi} className='btn-search'>Buscar personajes</button>
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">Rick & Morty </h1>
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters} />
+        ) : (
+          <>
+            <img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
+            <button onClick={reqApi} className="btn-search">
+              Buscar personajes
+            </button>
+          </>
+        )}
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- rick-morty/src/components/Characters.jsx
+++ rick-morty/src/components/Characters.jsx
@@ -1,74 +1,54 @@
-import PropTypes from "prop-types";
-
 export default function Characters(props) {
+  const { characters, setCharacters } = props;
 
-    // Desestructura
-    const { characters, setCharacters, setUrl, infoApi, setInfoApi } = props
-    console.log(characters)
+  const resetCharacters = () => {
+    setCharacters(null);
+  };
 
-    const resetCharacters = () => {
-        setCharacters(null);
-        console.log("reiniciando")
-    }
+  console.log(characters);
 
-    const nextPage = () => {
-        setUrl(infoApi.next);
-    }
-
-    const previousPage = () => {
-        setUrl(infoApi.prev)
-    }
-
-    return (
-        <div className="characters">
-            <h1>Personajes</h1>
-            <span className="back-home" onClick={resetCharacters}>Volver a la home</span>
-            <div className="container-characters">
-
-                {characters.map((character, index) => (
-                    // No utilizar un return dentro del map
-                    <div className="character-container" key={index}>
-                        <div>
-                            <img src={character.image} alt={character.name} />
-                        </div>
-
-                        <div>
-                            <h3>{character.name}</h3>
-                            <h6>
-                                {character.status === "Alive" ? (
-                                    <>
-                                        <span className="alive">Alive</span>
-                                    </>
-                                ) : (
-                                    <>
-                                        <span className="dead">Dead</span>
-                                    </>
-                                )}
-                            </h6>
-                            <p className="text-gray"><span>Episodios: </span><span>{character.episode.length}</span></p>
-                            <p>
-                                <span className="text-gray">Especie: </span><span>{character.species}</span>
-                            </p>
-                        </div>
-                    </div>
-                ))}
+  return (
+    <div className="characters">
+      <h1>Personajes</h1>
+      <span className="back-home" onClick={resetCharacters}>
+        Volver a la home
+      </span>
+      <div className="container-characters">
+        {characters.map((character, index) => (
+          <div className="character-container" key={index}>
+            <div>
+              <img src={character.image} alt={character.name} />
             </div>
             <div>
-                Ir a la pagina
-                <div>
-                    {infoApi.prev === null || <span className="btn-search" onClick={previousPage}>Anterior</span>}
-                </div>
-
-                <div>
-                    <span className="btn-search" onClick={nextPage}>Siguiente</span>
-                </div>
+              <h3>{character.name}</h3>
+              <h6>
+                {character.status === "Alive" ? (
+                  <>
+                    <span className="alive" />
+                    Alive
+                  </>
+                ) : (
+                  <>
+                    <span className="dead" />
+                    Dead
+                  </>
+                )}
+              </h6>
+              <p>
+                <span className="text-grey">Episodios: </span>
+                <span>{character.episode.length}</span>
+              </p>
+              <p>
+                <span className="text-grey">Especie: </span>
+                <span>{character.species}</span>
+              </p>
             </div>
-            <span className="back-home" onClick={resetCharacters}>Volver a la home</span>
-        </div>
-    )
-}
-
-Characters.propTypes = {
-    characters: PropTypes.array.isRequired,
-    setCharacters: PropTypes.func.isRequired
+          </div>
+        ))}
+      </div>
+      <span className="back-home" onClick={resetCharacters}>
+        Volver a la home
+      </span>
+    </div>
+  );
 }
